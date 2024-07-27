23c44cd81828eeb0562a5863e4d977216aee3012
sheraz4196 <sheraztoor321@gmail.com>
2024-07-27 16:01:17 +0500

    first commit

---
+++ watch-hub-storefront/src/app/layout.tsx
@@ -0,0 +1,20 @@
+import { Metadata } from "next"
+import "styles/globals.css"
+
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "https://localhost:8000"
+
+export const metadata: Metadata = {
+  metadataBase: new URL(BASE_URL),
+}
+
+export default function RootLayout(props: { children: React.ReactNode }) {
+  return (
+    <html lang="en" data-mode="light">
+      <body>
+        <main className="relative max-w-screen-2xl mx-auto">
+          {props.children}
+        </main>
+      </body>
+    </html>
+  )
+}
